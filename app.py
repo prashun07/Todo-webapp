5a392f4dfd1f86de68956b20e76f3adf6633cae3
Prashun Kumar <54842624+prashun07@users.noreply.github.com>
2021-08-24 10:08:22 +0530

    Comment Updated

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-app = Flask(__name__)
+app = Flask(__name__) # here we created instance of class Flask. '__name__' is a package which tells flask where to look resources like in static or template.
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -16,8 +16,11 @@ class Todo(db.Model):
     def __repr__(self):
         return f"{self.sno} - {self.title}"
 
-
-@app.route('/', methods=['GET', 'POST'])
+#GET and  POST is HTTP methods,by default methods have GET methods but other methods can also be added.
+#GET	It is the most common method which can be used to send data in the unencrypted form to the server.
+#POST	It is used to send the form data to the server. The server does not cache the data transmitted using the post method.
+#PUT	It is used to replace all the current representation of the target resource with the uploaded content.
+@app.route('/', methods=['GET', 'POST']) #route() in a decorator which tells what url will trigger function written below. 
 def input():
     if request.method == 'POST':
         title = request.form['title']
@@ -28,7 +31,7 @@ def input():
     allTodo = Todo.query.all()
     return render_template('index.html', allTodo=allTodo)
 
-
+#in <int:sno> sno is a variable where as int is type of convertible. there are 5 types of convertible in flask,int,string,float,path and uuid.
 @app.route('/update/<int:sno>', methods=['GET', 'POST'])
 def update(sno):
     if request.method == 'POST':
